75d29f79e3fa021d274ed0a6ef99c69305eea60d
Nelya <nailia.shakirova@gmail.com>
2016-12-19 11:32:59 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,45 +1,93 @@
- $(document).ready(function(){
-            
-            $(window).scroll(function() {
-                headerHeight = $("header").outerHeight();
-                w = $(window);               
-                if (($(document).scrollTop() > w.height() - headerHeight + 1) && ($(document).scrollTop() < w.height() + headerHeight + $("#about").height() + 1) )
-                
-                {
-                    $("nav").addClass("black");
-                    $("header").addClass("black");
-                    $("nav > a").css({
-                        backgroundColor: "#1a1a1a",
-                        color: "#ffffff"
-                    });      
-                } else 
-                {
-                    $("nav").removeClass("black");
-                    $("header").removeClass("black");
-                    $("nav > a").css({
-                        backgroundColor: "#ffffff",
-                        color: "#000000"
-                    });
-                }
+$(document).ready(function(){
+     
+    //changing styling of navigation bar, depends on scroll
+    $(window).scroll(function() {
+        headerHeight = $("header").height();
+        w = $(window);               
+        if (($(document).scrollTop() > -headerHeight + $("#home").outerHeight() + 1) &&
+            ($(document).scrollTop() <= -headerHeight + $("#home").outerHeight() + $("#about").outerHeight() + 1))
+
+        {
+            $("nav").addClass("black");
+            $("header").addClass("black");
+            $("nav > a").css({
+                backgroundColor: "#1a1a1a",
+                color: "#ffffff"
+            });      
+        } else {
+            $("nav").removeClass("black");
+            $("header").removeClass("black");
+            $("nav > a").css({
+                backgroundColor: "#ffffff",
+                color: "#000000"
             });
+        }   
+    });
+
+    //"scrollspy" effect - let's user know, where he is
+    $(window).scroll(function() {
+        headerHeight = $("header").height();
+        w = $(window);
+            if ($(document).scrollTop() <= -headerHeight + $("#home").outerHeight() + 1) {
+            $("a[href*='#']").removeClass("hovered");
+            $("a[href='#home']").addClass("hovered"); 
+        }
+        else if (($(document).scrollTop() > -headerHeight + $("#home").outerHeight() + 1) && 
+                 ($(document).scrollTop() <= -headerHeight + $("#home").outerHeight() + $("#about").outerHeight() + 1))  {
+            $("a[href*='#']").removeClass("hovered");
+            $("a[href='#about']").addClass("hovered"); 
+        }
+        else if (($(document).scrollTop() > -headerHeight + $("#home").outerHeight() + $("#about").outerHeight() + 1) &&
+                 ($(document).scrollTop() < headerHeight + $("main").outerHeight() - $(window).height()*0.6))  {
+            $("a[href*='#']").removeClass("hovered");
+            $("a[href='#portfolio']").addClass("hovered"); 
+        }
+        else if ($(document).scrollTop() > headerHeight + $("main").height() - $(window).height()*0.6)  {
+            $("a[href*='#']").removeClass("hovered");
+            $("a[href='#information']").addClass("hovered"); 
+        }
+    })
+    
+    
+    //opening footer with contacts section
+    $("main").css("margin-bottom", function(){
+                  return $(window).height() * 0.6;
+                  })
+
+    
+    //hover effect for navigation bar
+    $(".navigation").hover(
+        function(){
+            $(this).addClass("hovered");
+        },
+        function(){
+            $(this).removeClass("hovered");
+        }
+    );
+    
+    
+    
+    //smooth scrolling
+    $('a[href*="#"]:not([href="#"])').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
             
+            if (target.length) {
+                target = target;
+            } else {
+                target = $('[name="' + this.hash.slice(1) + '"]');
+            }
             
-            $("main").css("margin-bottom", function(){
-                          return $(window).height() * 0.6;
-                          })
+            if (target.length) {
+                $('html, body').animate({
+                    scrollTop: target.offset().top
+                }, 1000);
+                return false;
+            }
+        }
+    });
+    
+    
             
-            
-            $(".navigation").hover(
-                function(){
-                    $(this).addClass("red");
-                },
-                function(){
-                    $(this).removeClass("red");
-                }
-            );
-            
-            
-            
-            
-            
-        });
+});
+
